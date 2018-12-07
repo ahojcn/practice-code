0af177e29d8e2c2c31f1ed12d48ae5bf2b2262a7
ahojcn <hanoi_ahoj@icloud.com>
2018-12-08 00:45:04 +0800

    update_20181208

--- Test/20181206_C语言基础篇总结/20181206_C语言基础篇总结/main.c
+++ Test/20181206_C语言基础篇总结/20181206_C语言基础篇总结/main.c
@@ -3,8 +3,9 @@
 
 int main()
 {
-    printf("%lu\n", strlen("abcdef"));
-    printf("%lu\n", strlen("c:\test\32\test.c"));
-    
+//    printf("%lu\n", strlen("abcdef"));
+//    printf("%lu\n", strlen("c:\test\32\test.c"));
+
+    printf("%d\n", sizeof(long));
     return 0;
 }
