caea6d7ff9c0d9667a90c2647c012006eb59a220
ahojcn <hanoi_ahoj@icloud.com>
2018-12-30 20:27:50 +0800

    提交：数据结构课程设计优化 优化：代码复用，将一些常用的工具函数封装了起来； 增加了菜单

--- DataStructure/ClassDesign/main.c
+++ DataStructure/ClassDesign/main.c
@@ -2,29 +2,69 @@
 #include <string.h>
 #include <ctype.h>
 
-void ShowAns(const Answer *ans);
+typedef enum MenuSelect {
+    EXIT = 0,
+    NULLDECIMAL = 1,    // 选择，无小数
+    DECIMAL = 2,        // 有小数
+} MenuSelect;
+
+void ShowAnswer(
+        const char *bin,
+        const char *oct,
+        const char *dec,
+        const char *hex
+);
+void Menu(void );
+char *GetInput(void );
+void NullDecimal (char *input); // 处理无小数的输入
 
 int main()
 {
-    Answer ans; // 最终的结果将存在Ans中
+    while (1) {
+        Menu();
+        printf("select>");
+        MenuSelect ms = EXIT;
+        scanf("%d", &ms);
+        char *input = NULL;
+        switch(ms) {
+            case NULLDECIMAL:   // 无符号小数
+                input = GetInput();
+                NullDecimal(input);
+                break;
+            case DECIMAL:{
+                input = GetInput();
+                //
+                break;
+            }
 
-    char input[MAXLENGTH] = {0};
-    scanf("%s", input);
-    size_t input_len = strlen(input);
-
-    // 将输入的 input 转为大写，如果是大写则不转换
-    for (int i = 0; i < input_len; ++i) {
-        if (islower(input[i])) {
-            input[i] = (char)toupper(input[i]);
+            case EXIT:
+            default:
+                exit(0);    // 正常退出
+                break;
         }
     }
+
+    return 0;
+}
+
+void Menu(void ) {
+    printf("##################\n");
+    printf("##  1. 无小数\t##\n");
+    printf("##  2. 带小数\t##\n");
+    printf("##  0. 退出\t\t##\n");
+    printf("##################\n");
+}
+
+void NullDecimal (char *input) {
+    assert(input != NULL);
 
     char *b = NULL;
     char *o = NULL;
     char *d = NULL;
     char *h = NULL;
+    Answer ans;
     // 判断输入的是几进制的数,做相应的转换
-    switch(input[input_len-1]) {
+    switch(input[strlen(input)-1]) {
         case BIN:   // 输入为2进制 -> 转10进制 -> 转8、16进制
             ans.a_input_num_type = BIN;
             int bin_dec = AllToDec(input, 2);
@@ -70,11 +110,29 @@ int main()
             break;
     }
 
-    printf("bin:%s\n", b);
-    printf("oct:%s\n", o);
-    printf("dec:%s\n", d);
-    printf("hex:%s\n", h);
-
-    return 0;
+    ShowAnswer(b, o, d, h);
 }
 
+char *GetInput (void) {
+    char *input = (char *)malloc(MAXLENGTH);
+    printf("input>");
+    scanf("%s", input);
+
+    // 将输入的 input 转为大写，如果是大写则不转换
+    for (int i = 0; i < strlen(input); ++i) {
+        if (islower(input[i])) {
+            input[i] = (char)toupper(input[i]);
+        }
+    }
+    return input;
+}
+
+void ShowAnswer(const char *bin, const char *oct, const char *dec, const char *hex) {
+    assert(bin && oct && dec && hex != NULL);
+    printf("-----answer-----\n");
+    printf("bin: %s\n", bin);
+    printf("oct: %s\n", oct);
+    printf("dec: %s\n", dec);
+    printf("hex: %s\n", hex);
+    printf("-----answer-----\n");
+}
