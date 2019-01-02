d26e5b30018009b5fb4dc04ef81993fa93e09572
ahojcn <hanoi_ahoj@icloud.com>
2019-01-02 22:02:28 +0800

    提交：20190102练习、测试代码

--- Test/test_20181230_inBit/test_20181230_inBit/main.c
+++ Test/test_20181230_inBit/test_20181230_inBit/main.c
@@ -1,1 +1,36 @@
 
+//
+// Created by hanoi_ahoj on 2019/1/2.
+//
+
+#include <stdio.h>
+
+void fun(int a, int power)
+{
+    int tmp = a;
+    int count = 0;
+    char arr[100] = {0};
+    while (tmp)
+    {
+        arr[count] = tmp % power;
+        
+        tmp /= power;
+        
+        arr[count] += '0';
+        if (arr[count] > '9')
+        {
+            arr[count] += 7;
+        }
+        
+        count ++;
+    }
+    
+    printf("%s\n", arr);
+}
+
+int main()
+{
+    int a = 15;
+    fun(a, 8);
+    return 0;
+}
