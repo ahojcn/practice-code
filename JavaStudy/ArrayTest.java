0f2b0a310847db37c7ab72e809ea237c81c1a262
ahojcn <hanoi_ahoj@icloud.com>
2019-01-21 19:03:16 +0800

    1

--- JavaStudy/ArrayTest.java
+++
@@ -1,9 +0,0 @@
-class ArrayTest {
-	public static void main(String[] args) {
-		int[] arr = {1,2,3,4,5,6,7,8,9,11,22,33,44,55,66,77,88,99};
-		System.out.println("length of arr:" + arr.length);
-		for (int i = 0; i < arr.length; i ++) {
-			System.out.println(arr[i]);
-		}
-	}
-}
